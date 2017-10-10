a5b7e60ec07bd74efafeb5ac697bd2eee482c9f6
Jamatofu <clement.beal96@gmail.com>
2017-10-10 19:33:54 +0200

    Ajout d'un système de route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {LOCALE_ID, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import { NewsComponent } from './news/news.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatSidenavModule} from '@angular/material';
+import {MatSidenavModule, MatMenuModule} from '@angular/material';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { NewsListComponent } from './news-list/news-list.component';
 import {NewsService} from "../shared/news/news.service";
@@ -15,14 +15,25 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 import {AuthentificationService} from "../shared/authentification/authentification.service";
 import {HttpClientModule} from "@angular/common/http";
 import {NgPipesModule} from "ngx-pipes";
+import {RouterModule, Routes} from "@angular/router";
+import { CalendrierComponent } from './calendrier/calendrier.component';
 
+const appRoutes: Routes = [
+  { path: 'accueil', component: NewsListComponent },
+  // { path: 'produits', component: ProduitListComponent },
+  // { path: 'panier', component: PersonnalPanierComponent },
+  { path: '',   redirectTo: '/accueil', pathMatch: 'full' },
+  { path: 'calendrier', component: CalendrierComponent }
+  // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     NavbarComponent,
     NewsListComponent,
-    AuthentificationComponent
+    AuthentificationComponent,
+    CalendrierComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +44,8 @@ import {NgPipesModule} from "ngx-pipes";
     ReactiveFormsModule,
     FlexLayoutModule,
     NgPipesModule,
-    [MatSidenavModule]
+    [MatSidenavModule, MatMenuModule],
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [NewsService, AuthentificationService, {provide: LOCALE_ID, useValue: "fr-FR"}],
   bootstrap: [AppComponent]
